b9fb8be47824863ab82c016dc739d0f17b9e430a
fawwaz <farida.lhakim@gmail.com>
2022-06-01 05:55:06 +0700

    solved

--- main.py
+++ main.py
@@ -4,23 +4,8 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def home():
-    return render_template("home.html")
-
-
-@app.route("/about/")
-def about():
-    return render_template("about.html")
-
-
-@app.route("/about/profile/")
-def profile():
-    return render_template("profile_processor.html")
-
-
-@app.route("/products/")
-def products():
-    return render_template("produk_kopi.html")
+def main():
+    return render_template("main.html")
 
 
 if __name__ == "__main__":
